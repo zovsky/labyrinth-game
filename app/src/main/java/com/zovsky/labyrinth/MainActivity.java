eab9fe531e383b1e0086c5d7ea37a8ef8765ad36
zovsky <zovsky@gmail.com>
2016-03-20 23:52:54 +0300

    navigation view

--- app/src/main/java/com/zovsky/labyrinth/MainActivity.java
+++ app/src/main/java/com/zovsky/labyrinth/MainActivity.java
@@ -1,16 +1,35 @@
 package com.zovsky.labyrinth;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] mDrawerItems;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //mDrawerItems = getResources().getStringArray(R.array.planets_array);
+        //mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //mDrawerList = findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+//        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+//                R.layout.drawer_list_item, mPlanetTitles));
+//        // Set the list's click listener
+//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
     }
 
     @Override
