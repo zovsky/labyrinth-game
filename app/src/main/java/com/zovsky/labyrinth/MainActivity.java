1581df1c80610676aeed4d89920c40d4baf1e6a5
zovsky <zovsky@gmail.com>
2016-04-11 02:27:21 +0300

    start view pager

--- app/src/main/java/com/zovsky/labyrinth/MainActivity.java
+++ app/src/main/java/com/zovsky/labyrinth/MainActivity.java
@@ -1,27 +1,28 @@
 package com.zovsky.labyrinth;
 
-import android.app.Fragment;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
-import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.WebView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GameFragment.OnFragmentInteractionListener {
 
-    private String[] mDrawerItems;
     private DrawerLayout mDrawerLayout;
     private NavigationView mDrawerList;
     private Toolbar toolbar;
-    private WebView rules;
+    private Button startGame;
+    private Button showRules;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,30 +30,95 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-
-                return false;
-            }
-        });
-        toolbar.inflateMenu(R.menu.menu_main);
+//        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//
+//                return false;
+//            }
+//        });
+//        toolbar.inflateMenu(R.menu.menu_main);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        //mDrawerList = (NavigationView) findViewById(R.id.left_drawer);
-        //mDrawerItems = getResources().getStringArray(R.array.drawer_array);
+        mDrawerList = (NavigationView) findViewById(R.id.left_drawer);
+        mDrawerList.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        //Closing drawer on item click
+                        mDrawerLayout.closeDrawers();
 
+                        //Check to see which item was clicked and perform the appropriate action.
+                        switch (menuItem.getItemId()) {
 
-//        //Set the adapter for the list view
-//        mDrawerList.setAdapter(new ArrayAdapter<String>(this, null, mDrawerItems));
-//
-//        // Set the list's click listener
-//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-        WebView rules = (WebView) findViewById(R.id.rules_web_view);
-        String rules_str = getResources().getString(R.string.rules_html);
-        rules.loadDataWithBaseURL(null, rules_str, "text/html", "utf-8", null);    }
+                            case R.id.rules:
+                                showRulesWebView();
+                                return true;
+
+                            case R.id.new_game:
+                                showNewGameFragment();
+                                return true;
+
+                            default:
+                                return true;
+                        }
+                    }
+                });
+
+        showRules = (Button) findViewById(R.id.button_rules);
+        showRules.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRulesWebView();
+            }
+        });
+
+        startGame = (Button) findViewById(R.id.button_new);
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNewGameFragment();
+            }
+        });
+    }
+
+    private void showRulesWebView() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        Fragment fragment = new GameFragment();
+        ft.add(R.id.rules_web_view, fragment);
+        ft.addToBackStack("one");
+        //setTransition()
+        ft.commit();    }
+
+    private void showNewGameFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        Fragment fragment = new GameFragment();
+        ft.add(R.id.fragment_buttons, fragment);
+        ft.addToBackStack("one");
+        //setTransition()
+        ft.commit();
+    }
+
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            super.onBackPressed();
+        } else {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
