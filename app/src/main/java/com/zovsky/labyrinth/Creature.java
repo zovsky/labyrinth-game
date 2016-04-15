34bcfd323e4a6a47eda4de10f12d913ee4487e35
zovsky <zovsky@gmail.com>
2016-04-15 13:11:38 +0300

    creature class

---
+++ app/src/main/java/com/zovsky/labyrinth/Creature.java
@@ -0,0 +1,84 @@
+package com.zovsky.labyrinth;
+
+import java.util.ArrayList;
+
+public class Creature {
+
+    private int mL;
+    private int mV;
+    private int mU;
+    private String mElixir;
+    private ArrayList<String> mThings = new ArrayList<String>();
+    private int mGold;
+    private int mFood;
+    private ArrayList<Integer> mKeys = new ArrayList<Integer>();
+
+    public int getmL() {
+        return mL;
+    }
+
+    public void setmL(int mL) {
+        this.mL = mL;
+    }
+
+    public int getmV() {
+        return mV;
+    }
+
+    public void setmV(int mV) {
+        this.mV = mV;
+    }
+
+    public int getmU() {
+        return mU;
+    }
+
+    public void setmU(int mU) {
+        this.mU = mU;
+    }
+
+    public String getmElixir() {
+        return mElixir;
+    }
+
+    public void setmElixir(String mElixir) {
+        this.mElixir = mElixir;
+    }
+
+    public int getmGold() {
+        return mGold;
+    }
+
+    public void setmGold(int mGold) {
+        this.mGold = mGold;
+    }
+
+    public int getmFood() {
+        return mFood;
+    }
+
+    public void setmFood(int mFood) {
+        this.mFood = mFood;
+    }
+
+    public ArrayList<Integer> getmKeys() {
+        return mKeys;
+    }
+
+    public Creature() {
+        mL = 0;
+        mV = 0;
+        mU = 0;
+        mGold = 0;
+        mFood = 0;
+        mKeys.clear();
+        mThings.clear();
+        mElixir = "";
+    }
+
+
+
+
+}
+
+
