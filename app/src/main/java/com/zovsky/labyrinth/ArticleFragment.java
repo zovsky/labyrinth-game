d0cfca872fb814ee7e27c5e7c79f45f5c21d6eac
zovsky <zovsky@gmail.com>
2016-04-23 01:42:42 +0300

    i can edit menu!!

--- app/src/main/java/com/zovsky/labyrinth/ArticleFragment.java
+++ app/src/main/java/com/zovsky/labyrinth/ArticleFragment.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatRadioButton;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
