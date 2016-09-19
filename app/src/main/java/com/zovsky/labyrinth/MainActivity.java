5bae86f9989d406f26a054e4ad87a0627eeb554b
zovsky <zovsky@gmail.com>
2016-09-19 23:31:36 +0300

    303

--- app/src/main/java/com/zovsky/labyrinth/MainActivity.java
+++ app/src/main/java/com/zovsky/labyrinth/MainActivity.java
@@ -562,7 +562,7 @@ public class MainActivity extends AppCompatActivity
 
     public void takeSpecialAction(int article) {
         if (article == 16 || article == 47 || article == 61 || article == 68 || article == 137 ||
-                article == 248) {
+                article == 248 || article == 293 || article == 296) {
             //enable eatFood here
             if (gamePref.getInt("food", 0) > 0 && gamePref.getInt("foodTries", 0) > 0) {
                 setOneMenuItemActive("Запасы еды");
@@ -596,7 +596,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void takeAction(int article) {
-        if (article == 13 || article == 88 || article == 263) {
+        if (article == 13 || article == 88 || article == 263 || article == 291) {
             changeVVV(-2);
         }
         if (article == 15 || article == 157 || article == 181 || article == 216 || article == 329 || article == 377) {
