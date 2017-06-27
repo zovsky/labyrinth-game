ac5ddd1446a7185cfd1590ca218f8357477b4000
zovsky <zovsky@gmail.com>
2017-06-28 00:44:44 +0300

    clean imports

--- app/src/main/java/com/zovsky/labyrinth/PreBattleFragment.java
+++ app/src/main/java/com/zovsky/labyrinth/PreBattleFragment.java
@@ -6,21 +6,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.AppCompatRadioButton;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
